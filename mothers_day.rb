34e2dbc153d1f5a880eaa40d7bb4c98b59ea39ee
amulyamari <amulya@saimail.com>
2018-07-24 16:05:35 +0000

    Done.

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,3 +1,7 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
 
+def mothers_day(moms_name="mom")
+  puts "Happy Mother's day, #{moms_name}!"
+end
+mothers_day("beyonce")
