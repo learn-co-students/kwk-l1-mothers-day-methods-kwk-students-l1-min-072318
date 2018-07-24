38125b07fd658ec95b4aab68fbfed57ef8a85e60
cassidywalsh <cassidy.walsh19@blstudent.org>
2018-07-24 16:17:54 +0000

    Done.

---
+++ holiday_greeting.rb
@@ -0,0 +1,10 @@
+def holiday_greeting(name1 = "Mom",name2 = "Your Favorite Child",holiday = "Mother's Day")
+  puts "Happy #{holiday}, #{name1}! - From #{name2}"
+end
+
+holiday_greeting("Beyonce","Jay-Z","Fourth of July")
+holiday_greeting
+
+puts "Welcome to my holiday greeting program!"
+puts "Who is your greeting to?"
+gets ""
