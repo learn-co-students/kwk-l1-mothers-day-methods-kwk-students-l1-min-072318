1a8d32aa4aa51c404d40fb721adf71e828309539
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-24 16:57:48 +0000

    Done.

---
+++ holiday_greeting.rb
@@ -0,0 +1,5 @@
+def holiday_greeting(guest_name,your_name, holiday_name)
+  puts "Happy #{holiday_name}, #{guest_name}! - From #{your_name}"
+end
+
+holiday_greeting("Beyonce","Jay-Z","Fourth of July")
